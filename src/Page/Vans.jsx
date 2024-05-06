af4e6e5fb25717c3330ff343ea1183b3a6232879
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-05-06 17:27:31 +0100

    update

--- src/Page/Vans.jsx
+++ src/Page/Vans.jsx
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from "react";
 
 const Vans = () => {
-
-const [vansData, setVansData] = useState([])
+  const [vans, setVans] = useState([]);
 
   useEffect(() => {
     fetch("/api/vans")
       .then((response) => response.json())
-     /// .then((data) => console.log(data));
-     .then(data => setVansData(data.vansData))
+      /// .then((data) => console.log(data));
+      .then((data) => setVans(data.vans));
   }, []);
 
-console.log(vansData)
+  ///console.log(vansData)
+
+  const vanItems = vans.map(vandata => (
+    <div key={vandata.id} className="vantile">
+        <img src={vandata.imageUrl} />
+        <div className="vaninfo">
+            <h3>{vandata.name}</h3>
+            <p>${vandata.price}<span>/day</span></p>
+        </div>
+        <i className={`vantype ${vandata.type} selected`}>{vandata.type}</i>
+    </div>
+))
 
   return (
-    <div>vans page</div>
+    <div className="vancontainer">
+        <div className="vanlist">
+            {vanItems}
+        </div>
+    </div>
 )
-  
-};
-
+}
 export default Vans;
