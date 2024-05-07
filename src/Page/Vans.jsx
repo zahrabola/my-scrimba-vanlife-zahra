e2408a4e0f0fd827397430718cc02f603f06cc27
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-05-07 16:53:43 +0100

    Update vans

--- src/Page/Vans.jsx
+++ src/Page/Vans.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Vans = () => {
   const [vans, setVans] = useState([]);
@@ -9,27 +10,29 @@ const Vans = () => {
       /// .then((data) => console.log(data));
       .then((data) => setVans(data.vans));
   }, []);
+  // console.log(vans)
 
-  ///console.log(vansData)
-
-  const vanItems = vans.map(vandata => (
+  const vanItems = vans.map((vandata) => (
     <div key={vandata.id} className="vantile">
-        <img src={vandata.imageUrl} />
+      <Link to={`/vans/${vandata.id}`}>
+        <img src={vandata.imageUrl} alt="img" />
         <div className="vaninfo">
-            <h3>{vandata.name}</h3>
-            <p>${vandata.price}<span>/day</span></p>
+          <h3>{vandata.name}</h3>
+          <p>
+            ${vandata.price}
+            <span>/day</span>
+          </p>
         </div>
         <i className={`van-type ${vandata.type} selected`}>{vandata.type}</i>
+      </Link>
     </div>
-))
+  ));
 
   return (
     <div className="vancontainer">
-        <h1>Explore our van options</h1>
-        <div className="vanlist">
-            {vanItems}
-        </div>
+      <h1>Explore our van options</h1>
+      <div className="vanlist">{vanItems}</div>
     </div>
-)
-}
+  );
+};
 export default Vans;
