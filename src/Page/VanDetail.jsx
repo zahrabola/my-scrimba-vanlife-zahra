1741690acf0afba3d3a2fb6485d57ffbdfd9285a
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-05-07 17:30:23 +0100

    update

--- src/Page/VanDetail.jsx
+++ src/Page/VanDetail.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom"
 
 const VanDetail = () => {
+    const params = useParams()
+
+    const [vandetail, setVanDetail] = useState(null)
+
+    //console.log(params)
+
+
+    useEffect(() => {
+        fetch(`/api/vans/${params.id}`)
+        .then((response) => response.json())
+         ///.then((data) => console.log(data));
+         .then((data) => setVanDetail(data.vans));
+    }, [params.id])
+
+
+
     return (
-        <div>
-            detail
+        <div className='vancontainer'>
+           
+           { vandetail ? (
+            <div className='vandetail'>
+               <img src={vandetail.imageUrl} alt='img' />
+               <div className='vandetailbtn'>
+                    <i className={`van-type ${vandetail.type} selected`}>{vandetail.type}</i>
+                    </div>
+                    <h2>{vandetail.name}</h2>
+                    <p className="vanprice"><span>${vandetail.price}</span>/day</p>
+                    <p>{vandetail.description}</p>
+                    <button className="linkbutton">Rent this van</button>
+                </div>
+
+
+
+           ): <h1>Loading </h1>}
         </div>
     );
 }
