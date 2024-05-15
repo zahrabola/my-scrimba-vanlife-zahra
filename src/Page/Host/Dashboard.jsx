096a8cce86afa5670236f9cb63d5ecf3c2fd16db
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-05-15 14:41:55 +0100

    Update

--- src/Page/Host/Dashboard.jsx
+++ src/Page/Host/Dashboard.jsx
@@ -1,13 +1,84 @@
-import React from 'react';
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { BsStarFill } from "react-icons/bs";
+import { getHostVans } from "../../Data/Api";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <h1>Dashboard goes here</h1>
-         
+  const [vans, setVans] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    getHostVans()
+      .then((data) => setVans(data))
+      /// console.log(vans)
+      .catch((err) => setError(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  function renderVanDashboardItem(vans) {
+    const hostVanDashItem = vans.map((vandashdetail) => (
+     
+      <div className="hostvanwdash">
+             <div className="hostvansingle hostvanitem" key={vandashdetail.id}>
+        <img src={vandashdetail.imageUrl} alt={` ${vandashdetail.name}`} />
+        <div className="hostvaninfo">
+          <h3>{vandashdetail.name}</h3>
+          <p>${vandashdetail.price}/ day</p>
         </div>
+        <Link to={`vans/n${vandashdetail.id}`}>View</Link>
+      </div>
+        </div>
+     
+    ));
+
+    return (
+      <div className="hostvanslist">
+        <section>{hostVanDashItem} </section>
+      </div>
     );
-}
+  }
+
+  if (error) {
+    return <h1>Error: {error.message}</h1>;
+  }
+  return (
+    <div >
+      <section className="hostdashearnings">
+        <div className="dsashinfo">
+          <h1>Welcome</h1>
+          <p>
+            Income last <span>30 days</span>
+          </p>
+          <h2>$2,260</h2>
+        </div>
+        <Link to="income">Details</Link>
+      </section>
+      <section className="hostdashreviews">
+        <h2>Review score</h2>
+        <BsStarFill className="star" />
+        <p>
+          <span>5.0</span>/5
+        </p>
+        <Link to="reviews">Reviews</Link>
+      </section>
+      <section className="hostdashvans">
+        <div className="top">
+          <h2>Your listed vans</h2>
+          <Link to="vans">View all</Link>
+        </div>
+
+        {
+            loading && !vans ? <h1>Loading ... </h1> : (
+                <div className="renderdash">
+                    {renderVanDashboardItem(vans)}
+                </div>
+            )
+        }
+      </section>
+    </div>
+  );
+};
 
 export default Dashboard;
--- src/Page/Host/Income.jsx
+++ src/Page/Host/Income.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import graph from "../../Assets/graph.png";
 
 const Income = () => {
+
+    const transactionsData = [
+        { amount: 720, date: "Jan 3, '24", id: "1" },
+        { amount: 560, date: "Feb 12, '24", id: "2" },
+        { amount: 980, date: "May 3, '24", id: "3" },
+    ]
+    
     return (
         <div>
-           <h1>Income</h1> 
+            <section className='hostincome'>
+            <h1>Income</h1> 
+            <p>
+                Last <span>30 days</span>
+            </p>
+            <h2>$2,260</h2>
+
+            <img
+                className="graph"
+                src={graph}
+                alt="Income graph"
+            />
+            <div className='infoheader'>
+            <h3>Your transactions (3)</h3>
+                <p>
+                    Last <span>30 days</span>
+                </p>
+            </div>
+            <div className="renderdash">
+            {
+                transactionsData.map((tdata) => (
+                    <div key={tdata.id} className='transaction'>
+                        <h3>${tdata.amount}</h3>
+                        <p>{tdata.date}</p>
+                    </div>
+                ))
+            }
+            </div>
+            </section>
+          
         </div>
     );
 }
